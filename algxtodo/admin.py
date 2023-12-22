e4bb11908b4da99cea87ef7887cae0ea1f690518
Risav <79854041+risavnandi04@users.noreply.github.com>
2023-12-22 19:32:04 +0530

    Update admin.py

--- algxtodo/admin.py
+++ algxtodo/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Tag
 
-# Register your models here.
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "due_date")
+    list_filter = ("status",)
+    search_fields = ("title", "description")
+    fieldsets = (
+        (None, {"fields": ("title", "description", "status")}),
+        (
+            "Advanced options",
+            {
+                "classes": ("collapse",),
+                "fields": ("due_date", "tags"),
+            },
+        ),
+    )
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Tag)
