5283df4f726b4db69c2405fdf796a42ef3ea4d79
Risav <79854041+risavnandi04@users.noreply.github.com>
2023-12-22 19:32:32 +0530

    Update models.py

--- algxtodo/models.py
+++ algxtodo/models.py
@@ -4,9 +4,18 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 def validate_due_date(value):
     if value < timezone.now():
         raise ValidationError("The due date must be after the current time.")
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     STATUS_CHOICES = [
@@ -15,14 +24,13 @@ class Task(models.Model):
         ("DONE", "DONE"),
         ("OVERDUE", "OVERDUE"),
     ]
-    # breakpoint()
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
     due_date = models.DateTimeField(
         null=True, blank=True, validators=[validate_due_date]
     )
-    tags = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     status = models.CharField(
         max_length=7, choices=STATUS_CHOICES, default="OPEN"
     )
